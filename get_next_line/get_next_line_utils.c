56cc16480ffca9afec6758cdcda7e843a43e261d
Francisco Manuel <franciscomanueltoledo>
2024-10-23 18:58:43 +0200

    subida de get_next_line

--- get_next_line/get_next_line_utils.c
+++ get_next_line/get_next_line_utils.c
@@ -35,17 +35,56 @@ char	*ft_strchr(const char *str, int c)
 		return ((char*)&str[i]);
 	return ('\0');
 }
+
+char    *ft_strjoin(char const *s1, char const *s2){
+    int	i;
+	int len1;
+	int len2;
+	char *str;
+
+	if (s1 != '\0' && s2 != '\0')
+	{
+		len1 = strlen(s1);
+		len2 = strlen(s2);
+		str = (char*)malloc(sizeof(char) * (len1 + len2 + 1));
+		if (str == '\0')
+			return ('\0');
+		i = -1;
+		while (s1[++i] != '\0')
+			str[i] = s1[i];
+		while (s2[++i] != '\0')
+		{
+			str[len1] = s2[i];
+			len1++;
+		}
+		str[len1] = '\0';
+		return (str);
+	}
+	return('\0')
+}
+
 char ft_get_line(char lonlen)
 {
 	char	*str;
 	int i;
 
 	i = 0;
-	if (lonlen[i] != '\o')
+	if (lonlen[i] == '\0')
 		return ('\0');
+	str = (char*)malloc(sizeof(char)) * (i + 2);
+	if (!str)
+		return ('\0');
+	i = 0;
 	while (lonlen[i] != '\0' && lonlen[i] != '\n')
 	{
-		/* code */
+		str[i] = lonlen[i];
+		i++;
 	}
-	
+	if (lonlen[i] == '\n')
+	{
+		str[i] = lonlen[i];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
 }

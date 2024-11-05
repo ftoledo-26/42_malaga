be41c3202df8fa2e5e67961bfed9233f76ae8aa4
Francisco Manuel <franciscomanueltoledo>
2024-11-05 17:43:09 +0100

    asd

--- libft/libft.h
+++ libft/libft.h
@@ -272,7 +272,54 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 
 
 char    *ft_strdup(const char *s);
+
+
+/**
+ * 
+ */
+
 char    *ft_substr(char const *str, unsigned int star, size_t len);
 
+
+/**
+ * 
+ */
+
+char	*ft_itoa(int n);
+
+
+/**
+ * 
+ */
+
+void	ft_putnbr_fd(int n, int fd);
+
+
+/**
+ * 
+ */
+
+void	ft_putstr_fd(char *s, int fd);
+
+
+/**
+ * 
+ */
+
+void	ft_putchar_fd(char c, int fd);
+
+
+/**
+ * 
+ */
+
+void	ft_putendl_fd(char *s, int fd);
+
+
+/**
+ * 
+ */
+
+void	ft_putnbr_fd(int n, int fd);
 
 #endif

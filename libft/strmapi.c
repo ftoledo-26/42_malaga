5a9becc4ec76f8a8e81bef995b064d61e3edfd93
Francisco Manuel <franciscomanueltoledo@gmail.com>
2024-11-15 16:57:50 +0100

    libft

---
+++ libft/strmapi.c
@@ -0,0 +1,23 @@
+#include "libft.h"
+
+char	*ft_strmapi(char *s, char (*f)(unsigned int, char))
+{
+	int		i;
+	char	*str;
+	int		len;
+
+	i = 0;
+	if (!s)
+		return (NULL);
+	len = ft_strlen(s);
+	str = malloc(sizeof(char) * (len + 1));
+	if (!str)
+		return (NULL);
+	while (i < len)
+	{
+		str[i] = f(i, s[i]);
+		i++;
+	}
+	str[len] = '\0';
+	return (str);
+}

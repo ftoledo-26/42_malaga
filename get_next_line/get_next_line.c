b95eb604b35417d99ced9eab30905f1679467d9b
Francisco Manuel <franciscomanueltoledo@gmail.com>
2024-11-15 22:01:53 +0100

    get_next_line

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -1,9 +1,32 @@
 #include "get_next_line.h"
 
-char    *_fill_line_buffer(int fd, char *left_c, char *buffer)
+static char	*_fill_line_buffer(int fd, char *left_c, char *buffer)
 {
-    if (!fd)
-        return(NULL);
+	ssize_t	b_read;
+	char	*tmp;
+
+	b_read = 1;
+	while (b_read > 0)
+	{
+		b_read = read(fd, buffer, BUFFER_SIZE);
+		if (b_read == -1)
+		{
+			free(left_c);
+			return (NULL);
+		}
+		else if (b_read == 0)
+			break ;
+		buffer[b_read] = 0;
+		if (!left_c)
+			left_c = ft_strdup("");
+		tmp = left_c;
+		left_c = ft_strjoin(tmp, buffer);
+		free(tmp);
+		tmp = NULL;
+		if (ft_strchr(buffer, '\n'))
+			break ;
+	}
+	return (left_c);
 }
 
 char    *set_line(char *line_buffer)
@@ -16,8 +39,10 @@ char    *set_line(char *line_buffer)
         i++;
     if (line_buffer[i] == 0 || line_buffer[1] == 0)
         return(NULL);
-    left_c = ft_substr(line_buffer, i + 1, ft_strlen(line_buffer) - 1)
+    left_c = ft_substr(line_buffer, i + 1, ft_strlen(line_buffer) - 1);
     
+    line buffer[1 + i] = 0;
+    return (left_c);
 }
 
 char    *ft_get_next_line(int fd)
